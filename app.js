a5878bd3231ace69442bbafb08794d8fb67b3b77
kanhaiya-pms <kanhaiya.psa@postmortemshala.co.in>
2024-08-10 14:56:33 +0530

    fix src

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const chess = new Chess();
 let players = {};
 let currentPlayer = "w";
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
